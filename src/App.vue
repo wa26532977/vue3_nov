5550e4a81240a9bcb6dfa3a512a88387751ce2aa
jizhan <peterwang_123@yahoo.com>
2022-11-14 14:59:04 -0500

    first upload

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
+    <!--  <nav>-->
+    <!--    <router-link to="/">Home</router-link> |-->
+    <!--    <router-link to="/about">About</router-link>-->
+    <!--  </nav>-->
 
+    <header>
+        <h1>My Friends</h1>
+    </header>
+    <ul>
+        <friend-contact
+            v-for="friend in friends"
+            :key="friend.id"
+            :name="friend.name"
+            :phone-number="friend.phone"
+            :email-address="friend.email"
+            :is-favorite="friend.favorite"
+
+        ></friend-contact>
+<!--        <friend-contact name="Manuel Lorenz" phone-number="2012135" email-address="jlkajsdf@gmail.com" :is-favorite='true'></friend-contact>-->
+<!--        <friend-contact name="peter wang" phone-number="12012135" email-address="aaaakajsdf@gmail.com"></friend-contact>-->
+    </ul>
+</template>
+<script>
+import FriendContact from "@/views/FriendContact";
+
+export default {
+    name: "App",
+    components: {FriendContact},
+    data () {
+        return {
+            friends: [
+                {
+                    name: "Manuel",
+                    phone: "505213613131",
+                    email: "dsfasdf@gmail.com",
+                    favorite: true
+                },
+                {
+                    name: "1Manuel",
+                    phone: "1505213613131",
+                    email: "1dsfasdf@gmail.com"
+                }
+            ]
+        }
+    }
+
+}
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
---
+++ src/views/FriendContact.vue
@@ -0,0 +1,69 @@
+<template>
+    <li>
+        <h2>
+            {{ name }}
+            {{ displayFavorite }}
+        </h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? "Hide" : "show" }}</button>
+        <ul v-if="detailsAreVisible">
+            <li>
+                <strong>Phone:</strong>
+                {{ phoneNumber }}
+            </li>
+            <li>
+                <strong>Email:</strong>
+                {{ emailAddress }}
+            </li>
+
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    name: "FriendContact",
+    // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+    props: {
+        name: {
+            type: String,
+            required: true,
+            validator(value) {
+                return value.length > 5
+            }
+        },
+        phoneNumber: String,
+        emailAddress: String,
+        isFavorite: {
+            type: Boolean,
+            required: false,
+            default(rawProps) {
+                // console.log(rawProps)
+                return rawProps.name === 'peter wang';
+            }
+        },
+    },
+    data() {
+        return {
+            detailsAreVisible: false,
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible
+        }
+    },
+    computed: {
+        displayFavorite() {
+            // console.log(this.isFavorite)
+            if (this.isFavorite) {
+                return "(Favorite)"
+            }
+            return ""
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
